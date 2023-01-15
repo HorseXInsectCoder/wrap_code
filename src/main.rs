dd89c153470935ea1f0a4b6fbaa800b3ab5979e5
voyager-1 <cyhzero@gmail.com>
2023-01-15 20:34:27 +0800

    rests

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,86 @@
 use std::collections::HashMap;
 use std::env;
+use std::fs::create_dir;
+use serde_json::{json, Number, Value};
 use warp::Filter;
-
-const WEB_DIR: &str = "web/";
+use warp::reply::Json;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // 引入环境变量
     env::set_var("RUST_APP_LOG", "debug");
     pretty_env_logger::init_custom_env("RUST_APP_LOG");
 
-    let log = warp::log("basic");
+    let log = warp::log("apis");
 
-    // 这种方式同时能用于get和post。但用宏写会比较容易出bug，写比较简单的可以这么写
-    let greet = warp::path!("basic"/String/i32)
-        .map(|name, age| {
-            return format!("name: {}, age: {}", name, age); // 返回时要把i32转成string，因为i32没有实现 Reply trait
-        });
+    let hi = warp::get()
+        .and(warp::path("hello"))
+        .map(|| "hi");
 
-    // 注：map是同步的，and_then才是异步的
-    let add = warp::path!("add"/i32/i32)
-        .map(|a, b| return format!("res: {}", a + b));
+    // 直接用一个接口统一如 rest_get, rest_list, rest_create 这几个接口
+    let apis = hi.or(rest_api()).with(log);
 
-    // 不使用宏的方式，获取url参数
-    // path/$name?$a=$b
-    let items = warp::get()
-        .and(warp::path("items"))
-        .and(warp::path::param::<String>())
-        .and(warp::query::<HashMap<String, String>>())
-        .and(warp::path::end())
-        .and_then(get_items);
-
-    // 同时支持多个链接，且限定只能用get访问
-    let apis = warp::get().and(greet.or(add).or(items)).with(log);
-
-
-    let dir_static = warp::fs::dir(WEB_DIR);
-    let index = warp::get()
-        .and(warp::path::end())
-        .and(warp::fs::file(format!("{}/index.html", WEB_DIR)));
-    let static_route = dir_static.or(index);
-
-    let routes = static_route.or(apis);
-
-    warp::serve(routes).run(([127, 0, 0, 1], 8181)).await;
+    warp::serve(apis).run(([127, 0, 0, 1], 3000)).await;
 
     Ok(())
 }
 
-// 处理从url获取到的参数
-async fn get_items(
-    param: String,
-    param_map: HashMap<String, String>,
-) -> Result<impl warp::Reply, warp::Rejection> {
-    Ok(format!("get {}: {:?}", param, param_map))
+// 要返回实现了Filter的类型，且里面的范型要实现 Reply，错误是 Rejection，且必须是Clone的
+fn rest_api() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    let base_url = warp::path("rest");
+
+    // url: rest/xx
+    let get = base_url
+        .and(warp::get())
+        .and(warp::path::param())
+        .and_then(rest_get);
+
+    let list = base_url
+        .and(warp::get())
+        .and(warp::path::end())
+        .and_then(rest_list);
+
+    let create = base_url
+        .and(warp::post())
+        .and(warp::body::json())        // 从请求体读取JSON
+        .and_then(rest_create);
+
+
+    get.or(list).or(create)
+}
+
+// 注意用的是 warp::reply::Json;
+async fn rest_get(id: i32) -> Result<Json, warp::Rejection> {
+    let some_thing = json!({
+        "id": id,
+        "name": format!("name: {}", id)
+    });
+    let some_thing_warp = warp::reply::json(&some_thing);
+    Ok(some_thing_warp)
+}
+
+async fn rest_list() -> Result<Json, warp::Rejection> {
+    let res = json!([
+        {"id": 1,"status": "ok"},
+        {"id": 2,"status": "err"},
+        {"id": 3,"status": "aa"},
+        {"id": 4,"status": "bb"},
+        {"id": 5,"status": "cc"},
+    ]);
+
+    let some_thing_warp = warp::reply::json(&res);
+    Ok(some_thing_warp)
+}
+
+// 从请求体里接收JSON
+// 参数是 serde_json::Value;
+async fn rest_create(data: Value) -> Result<Json, warp::Rejection> {
+    let mut data: serde_json::Value = serde_json::from_value(data).unwrap();
+
+    let data_id: i32 = serde_json::from_value(data["id"].clone()).unwrap();
+    println!("{}", data_id);
+
+    data["id"] = Value::Number(Number::from(data_id + 1));
+
+    // 把接收到的JSON直接返回出去
+    Ok(warp::reply::json(&data))
 }
